c35ad093057fcc61b324782a7fa3838d7a6f01b9
Mário Gil <mg_mesquita@hotmail.com>
2018-03-01 20:05:56 +0000

    Check available car and assign trip

--- Testes Gil.py
+++ Testes Gil.py
@@ -1,9 +1,32 @@
-cars_list = []
+n_vehicles = 12
+car_list = [[0, (1,1), False] , [1, (0,0), False]]
+output_file = []
 
-number_of_cars = 12
+for car in range(n_vehicles): #Inicializa o output file
+    output_file.append([car])
 
-for car in range(number_of_cars):
-    cars_list.append([car, (0, 0), False])
+def availableCar():
+    for car in car_list:
+        if (car[2] == False):
+            return car[0]
+    return False
+
+def AssignNextTrip(carNumber, tripNumber):
+    for car in car_list:
+        if(car[0] == carNumber):
+            car[2] = True;
+            for line in output_file: #escrever no ficheiro de output o tripNumber
+                if (line[0] == carNumber):
+                    line.append(tripNumber)
+                    return
+            return
+
+tripNumber = 0
+while(availableCar()): #Verifies there is a car available
+    car = availableCar()
+    AssignNextTrip(car, tripNumber) #adicionar viagem, alterar valores dentro do carro
+    tripNumber += 1
 
 
-print (cars_list)
+print(output_file)
+print(car_list)
